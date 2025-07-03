e2a3a9e8cc5279e83cb126b7f800e44be10630ee
James Stables <james@neverguesswhat.com>
2025-07-03 16:55:49 +0100

    Initial Commit

---
+++ CafeMeetup/CafeMeetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeMeetupApp.swift
+//  CafeMeetup
+//
+//  Created by James Stables on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CafeMeetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CafeMeetupTests/CafeMeetupTests.swift
@@ -0,0 +1,16 @@
+//
+//  CafeMeetupTests.swift
+//  CafeMeetupTests
+//
+//  Created by James Stables on 03/07/2025.
+//
+
+import Testing
+
+struct CafeMeetupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
