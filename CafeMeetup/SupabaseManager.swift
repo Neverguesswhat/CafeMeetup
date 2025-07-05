e0cb17b025c3ef1f3d16dc17f2f416e85e007ebe
Neverguesswhat <james@neverguesswhat.com>
2025-07-05 12:51:23 +0100

    fix# Please enter the commit message for your changes. Lines starting

--- CafeMeetup/SupabaseManager.swift
+++ CafeMeetup/SupabaseManager.swift
@@ -13,7 +13,7 @@ class SupabaseManager {
         // TODO: Replace with proper environment variable or configuration
         client = SupabaseClient(
             supabaseURL: URL(string: "https://lhtudqxldzwloyyxgvrx.supabase.co")!,
-            supabaseKey: "your-key-here"
+            supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxodHVkcXhsZHp3bG95eXhndnJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MDU3NTcsImV4cCI6MjA2NTQ4MTc1N30.Qh34YH6mEn-2y2GB1TZ64xMFyn4bbEsS3nw2mDlOlro"
         )
     }
 
